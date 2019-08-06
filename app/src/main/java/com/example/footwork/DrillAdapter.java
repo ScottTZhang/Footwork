a47743d58407d22a7097236f90e25b3996997882
ScottTZhang <tingzhang2013@gmail.com>
2019-08-05 23:43:30 -0700

    Updated the adapter to use Drill array to carry data.

--- app/src/main/java/com/example/footwork/DrillAdapter.java
+++ app/src/main/java/com/example/footwork/DrillAdapter.java
@@ -7,24 +7,21 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
+
 
 public class DrillAdapter extends BaseAdapter {
 
-    private Context mContext;
-    private final String[] gridViewString; //would change to  drill item object
-    private final Integer[] gridViewImageId;
+    private final Context mContext;
+    private final Drill[] drills; //would change to  drill item object
 
-
-    DrillAdapter(Context context, String[] gridViewString, Integer[] gridViewImageId) {
+    DrillAdapter(Context context, Drill[] drills) {
         mContext = context;
-        this.gridViewImageId = gridViewImageId;
-        this.gridViewString = gridViewString;
+        this.drills = drills;
     }
 
     @Override
     public int getCount() {
-        return gridViewString.length;
+        return drills.length;
     }
 
     @Override
@@ -34,38 +31,25 @@ public class DrillAdapter extends BaseAdapter {
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        View gridViewDrill;
-        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        final Drill drill = drills[position];
 
         ImageView drillImageView;
         TextView drillTextView;
         if (convertView == null) {
-            gridViewDrill = inflater.inflate(R.layout.drill_gridview_layout, parent, false);
-            drillTextView = gridViewDrill.findViewById(R.id.drill_gridview_text);
-            drillImageView = gridViewDrill.findViewById(R.id.drill_gridview_image);
-            drillTextView.setText(gridViewString[position]);
-            drillImageView.setImageResource(gridViewImageId[position]);
-
-//            gridViewDrill.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//                    View textView = v.findViewById(R.id.drill_gridview_text);
-//                    textView.setVisibility(View.VISIBLE);
-//                    View imageView = v.findViewById(R.id.drill_gridview_image);
-//                    imageView.setVisibility(View.INVISIBLE);
-//                }
-//            });
-
-        } else {
-            gridViewDrill = convertView;
+            final LayoutInflater layoutInflater = LayoutInflater.from(mContext);
+            convertView = layoutInflater.inflate(R.layout.drill_gridview_layout, parent, false);
         }
+        drillTextView = convertView.findViewById(R.id.gridview_text);
+        drillImageView = convertView.findViewById(R.id.gridview_image);
+        drillTextView.setText(drill.title);
+        drillImageView.setImageResource(drill.imageId);
 
-        return gridViewDrill;
+        return convertView;
     }
 
 }
