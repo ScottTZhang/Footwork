fdcc358be1a95204664cdcf71fb8a7e9a63ebc36
ScottTZhang <tingzhang2013@gmail.com>
2019-08-05 23:41:50 -0700

    Created Drill, Difficulty class to bind data

---
+++ app/src/main/java/com/example/footwork/Difficulty.java
@@ -0,0 +1,11 @@
+package com.example.footwork;
+
+public class Difficulty {
+    String level;
+    Integer imageId;
+
+    public Difficulty(String lvl, Integer id) {
+        level = lvl;
+        imageId = id;
+    }
+}
---
+++ app/src/main/java/com/example/footwork/Drill.java
@@ -0,0 +1,12 @@
+package com.example.footwork;
+
+public class Drill {
+    String title, description;
+    Integer imageId;
+
+    public Drill(String t, String d, Integer id) {
+        title = t;
+        description = d;
+        imageId = id;
+    }
+}
