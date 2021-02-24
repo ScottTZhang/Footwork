7522ff27158e567aa0a4198c825293203fb15e78
ScottTZhang <tingzhang2013@gmail.com>
2021-02-24 00:46:04 -0800

    Hide the title in page, which is called Action Bar

--- app/src/main/java/com/example/footwork/DrillSelectActivity.java
+++ app/src/main/java/com/example/footwork/DrillSelectActivity.java
@@ -3,11 +3,15 @@ package com.example.footwork;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Objects;
+
 public class DrillSelectActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drill_select);
+        //hide action bar
+        Objects.requireNonNull(getSupportActionBar()).hide();
     }
 }
