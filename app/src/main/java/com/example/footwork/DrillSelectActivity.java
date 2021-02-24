f6a345e5ef9018a2fa89636585d2950539293d2e
ScottTZhang <tingzhang2013@gmail.com>
2021-02-24 00:25:20 -0800

    Create drill settings activity

---
+++ app/src/main/java/com/example/footwork/DrillSelectActivity.java
@@ -0,0 +1,13 @@
+package com.example.footwork;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DrillSelectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drill_select);
+    }
+}
