a793c85cf258c0933760d00f537a3308db2291ff
ScottTZhang <tingzhang2013@gmail.com>
2021-03-02 01:05:27 -0800

    Updated layout from ViewGroup to Relative

--- app/src/main/java/com/google/android/gms/samples/vision/face/facetracker/ui/camera/CameraSourcePreview.java
+++ app/src/main/java/com/google/android/gms/samples/vision/face/facetracker/ui/camera/CameraSourcePreview.java
@@ -22,8 +22,6 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-import android.view.ViewGroup;
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 import com.google.android.gms.common.images.Size;
